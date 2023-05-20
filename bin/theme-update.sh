ebde973fb6db319a729e5a83d293cfec19963bec
Hugo Ferreira <hugocf@users.noreply.github.com>
2023-05-20 01:57:45 +0100

    Scripts to setup and update themes

---
+++ bin/theme-update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# https://medium.com/factualopinions/consider-starting-all-your-bash-scripts-with-these-options-74fbec0cbb83
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd $SCRIPT_DIR/..
+
+git submodule update --remote --merge
