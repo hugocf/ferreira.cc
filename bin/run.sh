f470d218946216ff7cd2e72bf3e6f732c81a83a2
Hugo Ferreira <hugocf@users.noreply.github.com>
2023-05-20 01:58:26 +0100

    Script to run locally in dev mode

---
+++ bin/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# https://medium.com/factualopinions/consider-starting-all-your-bash-scripts-with-these-options-74fbec0cbb83
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd $SCRIPT_DIR/..
+
+(sleep 1 && open http://localhost:1313) &
+hugo --buildDrafts --buildExpired --buildFuture --printPathWarnings --panicOnWarning server
