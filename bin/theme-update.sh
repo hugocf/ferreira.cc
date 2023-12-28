41a47e6e38bb1f0bdf4b2d104b7049a8562362cf
Hugo Ferreira <hugocf@users.noreply.github.com>
2023-12-28 05:34:05 +0000

    Improve check for changes in theme update script

--- bin/theme-update.sh
+++ bin/theme-update.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 # https://medium.com/factualopinions/consider-starting-all-your-bash-scripts-with-these-options-74fbec0cbb83
-set -euo pipefail
+set -u
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd $SCRIPT_DIR/..
+cd "$SCRIPT_DIR"/.. || exit
 
 git submodule update --remote --merge
 
-time diff -ur themes/PaperMod/layouts/ layouts/ | grep -v "Only in themes"
+# Check differences
+diff -ur themes/PaperMod/layouts/ layouts/ | grep -v "Only in themes" > /tmp/theme-update-default.diff
+diff -u themes/PaperMod/layouts/_default/list.html layouts/snippets/list.html > /tmp/theme-update-snippets.diff
+
+# Report changes
+echo "Default overrides"
+diff "$SCRIPT_DIR"/theme-update-default.approved /tmp/theme-update-default.diff
+echo "Snippets list"
+diff "$SCRIPT_DIR"/theme-update-snippets.approved /tmp/theme-update-snippets.diff
