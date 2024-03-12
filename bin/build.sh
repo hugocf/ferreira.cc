e6a92a082b94092f24a231cab001f2536177663a
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-03-12 21:02:08 +0000

    Double quote script var to prevent globbing and word splitting

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd $SCRIPT_DIR/..
+cd "$SCRIPT_DIR"/..
 
 hugo --cleanDestinationDir --printPathWarnings --panicOnWarning
--- bin/run.sh
+++ bin/run.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd $SCRIPT_DIR/..
+cd "$SCRIPT_DIR"/..
 
 (sleep 1 && open http://localhost:1313) &
 hugo --buildDrafts --buildExpired --buildFuture server
