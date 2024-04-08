c2037d1802ac56be3d0b831883976c75a83382ec
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-04-08 01:09:05 +0100

    Revert "Ensure the build server uses the correct timezone"
    This reverts commit 7c84ed79616ccd7ff481c18aafe657cd2fca2d54.

--- bin/build.sh
+++ bin/build.sh
@@ -3,10 +3,6 @@
 # https://medium.com/factualopinions/consider-starting-all-your-bash-scripts-with-these-options-74fbec0cbb83
 set -euo pipefail
 
-echo $(date)
-export TZ="Europe/Lisbon"
-echo $(date)
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "$SCRIPT_DIR"/..
