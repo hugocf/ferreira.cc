37b75e358d1d60bbbf7b009e89d5a5157499773d
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-04-14 03:32:54 +0100

    Show `cp` command to approve theme changes/updates

--- bin/theme-compare.sh
+++ bin/theme-compare.sh
@@ -4,16 +4,22 @@
 set -u
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LAYOUTS_DEFAULT=theme-compare-default
+LAYOUTS_SNIPPETS=theme-compare-snippets
 
 cd "$SCRIPT_DIR"/.. || exit
 
 # Check differences
-diff -ur themes/PaperMod/layouts/ layouts/ | grep -v "Only in themes" > /tmp/theme-compare-default.diff
-diff -u themes/PaperMod/layouts/_default/list.html layouts/snippets/list.html > /tmp/theme-compare-snippets.diff
+diff -ur themes/PaperMod/layouts/ layouts/ | grep -v "Only in themes" > /tmp/$LAYOUTS_DEFAULT.diff
+diff -u themes/PaperMod/layouts/_default/list.html layouts/snippets/list.html > /tmp/$LAYOUTS_SNIPPETS.diff
 
 # Report changes
-echo "diff $SCRIPT_DIR/theme-compare-default.approved /tmp/theme-compare-default.diff"
-diff --color=always "$SCRIPT_DIR"/theme-compare-default.approved /tmp/theme-compare-default.diff
+echo "diff $SCRIPT_DIR/$LAYOUTS_DEFAULT.approved /tmp/$LAYOUTS_DEFAULT.diff"
+echo "? cp /tmp/$LAYOUTS_DEFAULT.diff $SCRIPT_DIR/$LAYOUTS_DEFAULT.approved"
+diff --color=always "$SCRIPT_DIR"/$LAYOUTS_DEFAULT.approved /tmp/$LAYOUTS_DEFAULT.diff
 
-echo "diff $SCRIPT_DIR/theme-compare-snippets.approved /tmp/theme-compare-snippets.diff"
-diff --color=always "$SCRIPT_DIR"/theme-compare-snippets.approved /tmp/theme-compare-snippets.diff
+echo
+
+echo "diff $SCRIPT_DIR/$LAYOUTS_SNIPPETS.approved /tmp/$LAYOUTS_SNIPPETS.diff"
+echo "? cp /tmp/$LAYOUTS_SNIPPETS.diff $SCRIPT_DIR/$LAYOUTS_SNIPPETS.approved"
+diff --color=always "$SCRIPT_DIR"/$LAYOUTS_SNIPPETS.approved /tmp/$LAYOUTS_SNIPPETS.diff
