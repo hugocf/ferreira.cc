012ecbb1f587edf69655f9fc0a97255a28b40bdc
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-04-20 03:09:31 +0100

    Refactor theme comparison script

--- bin/theme-compare.sh
+++ bin/theme-compare.sh
@@ -8,31 +8,41 @@
 # https://medium.com/factualopinions/consider-starting-all-your-bash-scripts-with-these-options-74fbec0cbb83
 set -u
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-LAYOUTS_DEFAULT=theme-compare-default
-LAYOUTS_SNIPPETS=theme-compare-snippets
-LAYOUTS_NOTICE=theme-compare-notice
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly SCRIPT_NAME=${0##*/}
+
+compare() {
+    local base_file=${SCRIPT_NAME%.*}-$1
+    local original=$2
+    local changed=$3
+
+    check_differences "$base_file" "$original" "$changed"
+    report_changes "$base_file"
+}
+
+check_differences() {
+    local base_file=$1
+    local original=$2
+    local changed=$3
+
+    if [ -d "$original" ]; then
+        diff --unified=0 --recursive "$original" "$changed" | grep -v "Only in _vendor"> /tmp/"$base_file".diff
+    else
+        diff --unified=0 "$original" "$changed" > /tmp/"$base_file".diff
+    fi
+}
+
+report_changes() {
+    local base_file=$1
+
+    echo "diff $SCRIPT_DIR/$base_file.approved /tmp/$base_file.diff"
+    echo "cp /tmp/$base_file.diff $SCRIPT_DIR/$base_file.approved"
+    diff --color=always "$SCRIPT_DIR"/"$base_file".approved /tmp/"$base_file".diff
+    echo
+}
 
 cd "$SCRIPT_DIR"/.. || exit
 
-# Check differences
-diff --unified=0 --recursive _vendor/github.com/adityatelange/hugo-PaperMod/layouts/ layouts/ | grep -v "Only in _vendor" > /tmp/$LAYOUTS_DEFAULT.diff
-diff --unified=0 _vendor/github.com/adityatelange/hugo-PaperMod/layouts/_default/list.html layouts/snippets/list.html > /tmp/$LAYOUTS_SNIPPETS.diff
-diff --unified=0 _vendor/github.com/martignoni/hugo-notice/layouts/shortcodes/notice.html layouts/shortcodes/notice.html > /tmp/$LAYOUTS_NOTICE.diff
-
-# Report changes
-echo "diff $SCRIPT_DIR/$LAYOUTS_DEFAULT.approved /tmp/$LAYOUTS_DEFAULT.diff"
-echo "? cp /tmp/$LAYOUTS_DEFAULT.diff $SCRIPT_DIR/$LAYOUTS_DEFAULT.approved"
-diff --color=always "$SCRIPT_DIR"/$LAYOUTS_DEFAULT.approved /tmp/$LAYOUTS_DEFAULT.diff
-
-echo
-
-echo "diff $SCRIPT_DIR/$LAYOUTS_SNIPPETS.approved /tmp/$LAYOUTS_SNIPPETS.diff"
-echo "? cp /tmp/$LAYOUTS_SNIPPETS.diff $SCRIPT_DIR/$LAYOUTS_SNIPPETS.approved"
-diff --color=always "$SCRIPT_DIR"/$LAYOUTS_SNIPPETS.approved /tmp/$LAYOUTS_SNIPPETS.diff
-
-echo
-
-echo "diff $SCRIPT_DIR/$LAYOUTS_NOTICE.approved /tmp/$LAYOUTS_NOTICE.diff"
-echo "? cp /tmp/$LAYOUTS_NOTICE.diff $SCRIPT_DIR/$LAYOUTS_NOTICE.approved"
-diff --color=always "$SCRIPT_DIR"/$LAYOUTS_NOTICE.approved /tmp/$LAYOUTS_NOTICE.diff
+compare default  _vendor/github.com/adityatelange/hugo-PaperMod/layouts/                   layouts/
+compare snippets _vendor/github.com/adityatelange/hugo-PaperMod/layouts/_default/list.html layouts/snippets/list.html
+compare notice   _vendor/github.com/martignoni/hugo-notice/layouts/shortcodes/notice.html  layouts/shortcodes/notice.html
