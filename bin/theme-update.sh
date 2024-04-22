09e8b9bba69e22b739a627228a8b6bf3a587fe93
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-04-22 17:22:16 +0100

    Also vendor the updated modules in the theme update script

--- bin/theme-update.sh
+++ bin/theme-update.sh
@@ -8,5 +8,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"/.. || exit
 
 hugo mod get -u github.com/adityatelange/hugo-PaperMod
+hugo mod vendor
 
 "$SCRIPT_DIR"/theme-compare.sh
