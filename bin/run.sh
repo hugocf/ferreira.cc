5d896ccdfd1be82ff7ee6734d8717afcf58eb95d
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-05-19 00:27:29 +0100

    Ensure local server run cleans old content

--- bin/run.sh
+++ bin/run.sh
@@ -8,4 +8,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"/..
 
 (sleep 1 && open http://localhost:1313) &
-hugo --buildDrafts --buildExpired --buildFuture server
+hugo --cleanDestinationDir --buildDrafts --buildExpired --buildFuture server
